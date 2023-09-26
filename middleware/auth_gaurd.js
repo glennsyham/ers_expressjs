f510bdaae2d5fcfd66e820d6dedf6940171289dd
Glenn Syham <glennsyham@gmail.com>
2023-09-25 18:10:10 -0700

    removed unnecesary code

--- middleware/auth_gaurd.js
+++ middleware/auth_gaurd.js
@@ -6,27 +6,20 @@ const jwtUtil = require('../utility/jwt_util');
 const authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (authHeader) {
-
-
-        try {
-            const token = authHeader.split(' ')[1];
-            jwtUtil.verifyTokenAndReturnPayload(token)
-                .then((payload) => {
-                    req.user = payload;
-                    next();
+        const token = authHeader.split(' ')[1];
+        jwtUtil.verifyTokenAndReturnPayload(token)
+            .then((payload) => {
+                req.user = payload;
+                next();
+            })
+            .catch((err) => {
+                console.error(err);
+                res.statusCode = 401;
+                res.send({
+                    message: "Failed to Authenticate Token"
                 })
-                .catch((err) => {
-                    console.error(err);
-                    res.statusCode = 401;
-                    res.send({
-                        message: "Failed to Authenticate Token"
-                    })
-                })
+            })
 
-
-        } catch (error) {
-            return res.sendStatus(403);
-        }
     } else {
         res.sendStatus(401);
     }
